669ff085501bea5b3ce0f88f763577ef424f03c8
Yiding Han <jenny.han1989@gmail.com>
2020-06-14 16:52:14 -0700

    Added documentation for class

--- CalendarDataManager.py
+++ CalendarDataManager.py
@@ -1,1 +1,11 @@
+"""This class manages data that pulled from Google calendar and provide data for GUI"""
+
+# -----------------------------------------------------------------------------------------------------------------------
+# Name:       CalendarDataManager.py
+# Purpose:    This class pulls calendar data from Google calendar and store it in a list, for later use on GUI
+# Author:     Yiding Han
+# Created:    6/12/2020
+# TODO:       Add functions
+# Note:
+# -----------------------------------------------------------------------------------------------------------------------
 
