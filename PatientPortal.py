a7b6c388562900bfed416d299c2a7e061dd0ece1
Yiding Han <jenny.han1989@gmail.com>
2020-06-14 16:53:24 -0700

    Added documentation for class

--- PatientPortal.py
+++ PatientPortal.py
@@ -1,1 +1,12 @@
+"""This is the main class that coordinates the backend(CalendarDataManager) and GUI(CalendarGUI)"""
+
+# -----------------------------------------------------------------------------------------------------------------------
+# Name:       PatientPortal.py
+# Purpose:    Coordinated the backend and GUI
+# Author:     Yiding Han
+# Created:    6/12/2020
+# TODO:       Add functions
+# Note:
+# -----------------------------------------------------------------------------------------------------------------------
+
 
