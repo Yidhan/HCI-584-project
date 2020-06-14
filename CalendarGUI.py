7f77bca530004e6be526522586d09592a011a7e3
Yiding Han <jenny.han1989@gmail.com>
2020-06-14 16:52:41 -0700

    Added documentation for class

--- CalendarGUI.py
+++ CalendarGUI.py
@@ -1,1 +1,12 @@
+"""This is the GUI class that when interact directly with user. User interactions include looking up available time
+and request booking. """
+
+# -----------------------------------------------------------------------------------------------------------------------
+# Name:       CalendarGUI.py
+# Purpose:    This is the patient GUI where patients look up available time slots and request booking.
+# Author:     Yiding Han
+# Created:    6/12/2020
+# TODO:       Add functions
+# Note:
+# -----------------------------------------------------------------------------------------------------------------------
 
