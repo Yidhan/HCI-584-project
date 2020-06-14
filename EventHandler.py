294acec7429dbfb6ee0c0d84246b9dcdb957328b
Yiding Han <jenny.han1989@gmail.com>
2020-06-14 16:53:06 -0700

    Added documentation for class

--- EventHandler.py
+++ EventHandler.py
@@ -1,1 +1,12 @@
+"""This is the class that Handles all the GUI events, such as button pressed, key pressed, etc. It handles users
+actions on GUI. """
+
+# -----------------------------------------------------------------------------------------------------------------------
+# Name:       EventHandler.py
+# Purpose:    This class handles the GUI events, it handles user actions on GUI.
+# Author:     Yiding Han
+# Created:    6/12/2020
+# TODO:       Add functions
+# Note:
+# -----------------------------------------------------------------------------------------------------------------------
 
