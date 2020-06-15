90cccbc46b3fb5eabafa9f0fa847b434dd44f877
Yiding Han <jenny.han1989@gmail.com>
2020-06-14 17:16:49 -0700

    Created CalendarDataManager class, declared two functions and a instance variable

--- CalendarDataManager.py
+++ CalendarDataManager.py
@@ -5,7 +5,19 @@
 # Purpose:    This class pulls calendar data from Google calendar and store it in a list, for later use on GUI
 # Author:     Yiding Han
 # Created:    6/12/2020
-# TODO:       Add functions
+# TODO:       Add function body
 # Note:
 # -----------------------------------------------------------------------------------------------------------------------
+
+from datetime import datetime,timedelta, timezone
+
+class CalendarDataManager(object):
+
+    event_list = [] #class attribute
+    #get data from google calendar
+    def get_calendar_data(url):
+        pass
+    #parse data to gui, each timeslot(list memeber) as buttons
+    def parse_to_gui(event_list):
+        pass
 
